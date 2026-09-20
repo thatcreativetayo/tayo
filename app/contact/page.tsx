514863d9da32ac4a9779a07ec541558b91774b7f
therealteejay25 <therealteejay25@gmail.com>
2026-09-20 22:44:37 +0100

    random

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,9 @@ export default function ContactPage() {
   return (
     <div className="flex flex-col h-full">
       <Navbar />
-      <div className="pt-24 md:pt-32">
+       <div className="flex scroll-smooth mt-20 relative flex-col h-full items-center ">
+            <div className="hidden lg:block h-full min-h-screen z-20 absolute left-40 border text-black/10 border-long-dashed"></div>
+      <div className="hidden lg:block h-full min-h-screen z-20 absolute right-40 border text-black/10 border-long-dashed"></div>
         <Contact />
       </div>
     </div>
